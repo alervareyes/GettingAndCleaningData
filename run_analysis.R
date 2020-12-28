5c0e19f68cce23e4f458aa006a64b3933d99dda4
alervareyes <73333390+alervareyes@users.noreply.github.com>
2020-12-28 20:04:34 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,20 +1,20 @@
 library(dplyr)
 
 # read train data
-x_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
-y_train <- read.table("./UCI HAR Dataset/train/Y_train.txt")
-subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+x_train <- read.table("C://Users/nabreyes/Downloads/UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("C://Users/nabreyes/Downloads/UCI HAR Dataset/train/Y_train.txt")
+subject_train <- read.table("C://Users/nabreyes/Downloads/UCI HAR Dataset/train/subject_train.txt")
 
 # read test data
-x_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
-y_test <- read.table("./UCI HAR Dataset/test/Y_test.txt")
-subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+x_test <- read.table("C://Users/nabreyes/Downloads/UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("C://Users/nabreyes/Downloads/UCI HAR Dataset/test/Y_test.txt")
+subject_test <- read.table("C://Users/nabreyes/Downloads/UCI HAR Dataset/test/subject_test.txt")
 
 # read data description
-varnames <- read.table("./UCI HAR Dataset/features.txt")
+varnames <- read.table("C://Users/nabreyes/Downloads/UCI HAR Dataset/features.txt")
 
 # read activity labels
-actlabels <- read.table("./UCI HAR Dataset/activity_labels.txt")
+actlabels <- read.table("C://Users/nabreyes/Downloads/UCI HAR Dataset/activity_labels.txt")
 
 # 1. Merges the training and the test sets to create one data set.
 x_merge <- rbind(x_train, x_test)
@@ -22,7 +22,7 @@ y_merge <- rbind(y_train, y_test)
 subject_merge <- rbind(subject_train, subject_test)
 
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-select_var <- variable_names[grep("mean\\(\\)|std\\(\\)",variable_names[,2]),]
+select_var <- varnames[grep("mean\\(\\)|std\\(\\)",varnames[,2]),]
 x_merge <- x_merge[,select_var[,1]]
 
 # 3. Uses descriptive activity names to name the activities in the data set
@@ -37,4 +37,4 @@ colnames(x_merge) <- varnames[select_var[,1],2]
 colnames(subject_merge) <- "subject"
 total <- cbind(x_merge, activitylabel, subject_merge)
 total_mean <- total %>% group_by(activitylabel, subject) %>% summarize_each(funs(mean))
-write.table(total_mean, file = "./UCI HAR Dataset/tidydata.txt", row.names = FALSE, col.names = TRUE)
+write.table(total_mean, file = "C://Users/nabreyes/Downloads/UCI HAR Dataset/tidydata.txt", row.names = FALSE, col.names = TRUE)
