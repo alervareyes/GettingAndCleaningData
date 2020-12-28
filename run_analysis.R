f80e2deeb43d7989d88f2be1f908043a0252f818
alervareyes <73333390+alervareyes@users.noreply.github.com>
2020-12-28 19:49:24 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -19,7 +19,7 @@ actlabels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 # 1. Merges the training and the test sets to create one data set.
 x_merge <- rbind(x_train, x_test)
 y_merge <- rbind(y_train, y_test)
-sub_merge <- rbind(sub_train, sub_test)
+subject_merge <- rbind(subject_train, subject_test)
 
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 select_var <- variable_names[grep("mean\\(\\)|std\\(\\)",variable_names[,2]),]
@@ -34,7 +34,7 @@ activitylabel <- y_merge[,-1]
 colnames(x_merge) <- varnames[select_var[,1],2]
 
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-colnames(sub_merge) <- "subject"
-total <- cbind(x_merge, activitylabel, sub_merge)
+colnames(subject_merge) <- "subject"
+total <- cbind(x_merge, activitylabel, subject_merge)
 total_mean <- total %>% group_by(activitylabel, subject) %>% summarize_each(funs(mean))
 write.table(total_mean, file = "./UCI HAR Dataset/tidydata.txt", row.names = FALSE, col.names = TRUE)
